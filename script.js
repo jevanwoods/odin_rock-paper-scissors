2d26715636a4edb2406576f67733f4046dd8024c
Evan <j.evan.woods@gmail.com>
2023-01-06 11:08:59 -0500

    Created random computer choice in Rk Pp Srs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Let's see if this works.
+// Make an array giving the choices for the computer
+
+var choices = ["ROCK", "PAPER", "SCISSORS"];
+
+// Make a function that lets the computer pick randomly from the array
+
+function getComputerChoice(choices) {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+// Log that decision in console to assure it's functioning
+
+console.log(getComputerChoice(choices));
+
