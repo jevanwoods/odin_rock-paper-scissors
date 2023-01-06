f1e372133bbc957ba144ddfadd266969098401ab
Evan <j.evan.woods@gmail.com>
2023-01-06 11:53:12 -0500

    Added player selection and play function.

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 // Make an array giving the choices for the computer
-
 var choices = ["ROCK", "PAPER", "SCISSORS"];
 
 // Make a function that lets the computer pick randomly from the array
-
-function getComputerChoice(choices) {
-    return choices[Math.floor(Math.random()*choices.length)];
-}
+let computerChoice = choices[Math.floor(Math.random()*choices.length)];
 
 // Log that decision in console to assure it's functioning
+console.log(computerChoice);
 
-console.log(getComputerChoice(choices));
+// Make a prompt to get the players choice
+var playerChoice = prompt("Rock, Paper, or Scissors?");
 
+// Make playerChoice uniform for comparison
+let uniformChoice = playerChoice.toUpperCase();
+
+// Log that player choice to assure it's functioning
+console.log(uniformChoice);
+
+// Compare two choices
+function play(uniformChoice, computerChoice) {
+    if (uniformChoice === computerChoice) {
+        console.log("Tie");
+    } else if (uniformChoice === "ROCK" && computerChoice === "PAPER") {
+        console.log("You lose");
+    } else if (uniformChoice === "ROCK" && computerChoice === "SCISSORS") {
+        console.log("You win");
+    } else if (uniformChoice === "PAPER" && computerChoice === "SCISSORS") {
+        console.log("You lose");
+    } else if (uniformChoice === "PAPER" && computerChoice === "ROCK") {
+        console.log("You win");
+    } else if (uniformChoice === "SCISSORS" && computerChoice === "ROCK") {
+        console.log("You lose");
+    } else if (uniformChoice === "SCISSORS" && computerChoice === "PAPER") {
+        console.log("You win");
+    } else {
+        console.log("Invalid Entry");
+    }
+}
+
+// Play the game
+play(uniformChoice, computerChoice);
